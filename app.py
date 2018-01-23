cdf336a2f9b0bc24c1f5b4e6d3ddc72584b4d7ba
Pavel Bazin <pvlbzn@gmail.com>
2018-01-23 03:00:10 +0100

    update

--- app.py
+++ app.py
@@ -18,8 +18,9 @@
 from random_process import MarkovChain, parse_user_input
 from flask import Flask, url_for
 
+
 app = Flask(__name__)
-markov_chain = MarkovChain('./data/data_1515521742.csv')
+markov_chain = MarkovChain('./data/data.csv')
 
 
 @app.route('/api/v1/trump/<string:umsg>', methods=['GET'])
@@ -46,7 +47,6 @@ from flask import render_template
 @app.route('/', methods=['GET'])
 def main():
     return render_template('main.html', name='Vyasa')
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=26000)
